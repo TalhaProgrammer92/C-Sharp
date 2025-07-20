7d74d628429fa29092b834a9b13b2b50c9af5c92
TalhaProgrammer92 <talha.code92@gmail.com>
2025-07-20 14:18:49 +0500

    /

---
+++ Console/Projects/Chess/Chess/GamePlayer/Name.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.GamePlayer
+{
+    class Name
+    {
+        // Attributes
+        string name;
+
+        // Constructor
+        public Name(string name)
+        { this.name = name; }
+
+        // Getter
+        public string get() { return name; }
+
+        // Check if name is valid
+        public static bool isValid(string name)
+        {
+            return !(name.Length < 3 || string.IsNullOrEmpty(name));
+        }
+
+        // Check if two names are same
+        public static bool areEqual(string name1, string name2)
+        {
+            // If both strings have different length
+            if (name1.Length !=  name2.Length) return false;
+
+            // Compare each character of both string (not case sensitive)
+            for (int i = 0; i < name1.Length; i++)
+            {
+                if (char.ToLower(name1[i]) != char.ToLower(name2[i]))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Console/Projects/Chess/Chess/Program.cs
@@ -0,0 +1,17 @@
+﻿using Chess.GamePlayer;
+
+namespace Chess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            bool valid = Name.isValid("DON");
+            Console.WriteLine($"Valid: {valid}");
+
+            bool equal = Name.areEqual("DON", "dan");
+            Console.WriteLine($"Equal: {equal}");
+        }
+    }
+}
