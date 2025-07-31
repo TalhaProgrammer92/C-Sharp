046e61d435e4d7c7e40dee9334240a53dc1c1c27
TalhaProgrammer92 <talha.code92@gmail.com>
2025-07-31 17:57:05 +0500

    / .

---
+++ Console/Projects/Chess/Chess/Pieces/Pawn.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess.MiscUtils;
+
+namespace Chess.Pieces
+{
+    class Pawn : Piece
+    {
+        // Constructor
+        public Pawn(string symbol) : base(symbol)
+        {
+            isSymbolChangeable = true; // Pawns can change their symbol when they promote
+        }
+        
+        // Method - Is valid destination
+        public bool IsValidDestination(Position destination)
+        {
+            // Logic to check if the destination is valid for a pawn
+            // For simplicity, let's assume pawns can only move one step forward
+            // and can capture diagonally.
+            Position absDifference = Position.GetAbsoluteDifference(CurrentPosition, destination);
+
+            return  (absDifference.Row == 1 && absDifference.Column == 0) ||    // Move forward (Top to Bottom)
+                    (absDifference.Row == 0 && absDifference.Column == 1) ||    // Move forword (Bottom to Top)
+                    (absDifference.Row == 1 && absDifference.Column == 1);      // Capture diagonally
+        }
+    }
+}
---
+++ Console/Projects/Chess/Chess/Pieces/Piece.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chess.MiscUtils;
+
+namespace Chess.Pieces
+{
+    abstract class Piece
+    {
+        // Attributes
+        protected string symbol;
+        protected bool isSymbolChangeable = false;
+
+        public Position CurrentPosition { get; set; }
+        public Position RecentPosition { get; set; }
+
+        // Constructor
+        public Piece(string symbol)
+        {
+            this.symbol = symbol;
+        }
+
+        // Getter and Setter - Symbol
+        public string Symbol
+        {
+            get { return symbol; }
+            set { if (isSymbolChangeable) symbol = value; }
+        }
+
+        // Method - Print the piece
+        public void Print()
+        {
+            Console.WriteLine($"Piece: {symbol}, Current Position: {CurrentPosition.Row}{CurrentPosition.Column}, Recent Position: {RecentPosition.Row}{RecentPosition.Column}");
+        }
+
+        // Method - Update the piece position to a new position
+        protected void UpdatePosition(Position newPosition)
+        {
+            RecentPosition = new Position(CurrentPosition);
+            CurrentPosition = new Position(newPosition);
+        }
+    }
+}
