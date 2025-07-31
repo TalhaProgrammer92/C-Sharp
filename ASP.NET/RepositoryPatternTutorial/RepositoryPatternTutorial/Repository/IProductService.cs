6390d1ab964d12d713310308452766292197ec4d
TalhaProgrammer92 <talha.code92@gmail.com>
2025-07-31 18:46:27 +0500

    .

---
+++ ASP.NET/RepositoryPatternTutorial/RepositoryPatternTutorial/Repository/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using RepositoryPatternTutorial.Models;
+
+namespace RepositoryPatternTutorial.Repository
+{
+    public interface IProductService
+    {
+        public IEnumerable<Product> GetProducts();
+        public Product PostProduct(Product product);
+    }
+}
---
+++ ASP.NET/RepositoryPatternTutorial/RepositoryPatternTutorial/Repository/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using RepositoryPatternTutorial.Data;
+using RepositoryPatternTutorial.Models;
+
+namespace RepositoryPatternTutorial.Repository
+{
+    public class ProductService : IProductService
+    {
+        ProductContext context;
+
+        public ProductService(ProductContext context) 
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Product> GetProducts()
+        {
+            return context.Products.ToList();
+        }
+
+        public Product PostProduct(Product product)
+        {
+            context.Products.Add(product);
+            context.SaveChangesAsync();
+
+            return product;
+        }
+    }
+}
