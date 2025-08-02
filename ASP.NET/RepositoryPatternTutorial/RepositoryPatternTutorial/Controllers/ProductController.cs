7eec5726be2d84099cffa2834715b713d41337b8
TalhaProgrammer92 <talha.code92@gmail.com>
2025-08-02 07:53:29 +0500

    .

--- ASP.NET/RepositoryPatternTutorial/RepositoryPatternTutorial/Controllers/ProductController.cs
+++ ASP.NET/RepositoryPatternTutorial/RepositoryPatternTutorial/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RepositoryPatternTutorial.Models;
+using RepositoryPatternTutorial.Repository;
 
 namespace RepositoryPatternTutorial.Controllers
 {
@@ -7,5 +9,27 @@ namespace RepositoryPatternTutorial.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        // Attributes
+        IProductService productService;
+
+        // Constructor
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        // Get all products
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return productService.GetProducts();
+        }
+
+        // Post a new product
+        [HttpPost]
+        public Product PostProduct(Product product)
+        {
+            return productService.PostProduct(product);
+        }
     }
 }
--- ASP.NET/RepositoryPatternTutorial/RepositoryPatternTutorial/Program.cs
+++ ASP.NET/RepositoryPatternTutorial/RepositoryPatternTutorial/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace RepositoryPatternTutorial
 {
     public class Program
@@ -9,10 +11,19 @@ namespace RepositoryPatternTutorial
 
             // Add services to the container.
 
+            // Configure Entity Framework Core to use a SQL Server database
+            builder.Services.AddDbContext<Data.ProductContext>(options =>
+                // Configure the database context to use SQL Server
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseSettings") ??
+                
+                // If the connection string is not found, throw an exception
+                throw new InvalidOperationException("Connection string not found!")));
+
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddScoped<Repository.IProductService, Repository.ProductService>();
 
             var app = builder.Build();
 
