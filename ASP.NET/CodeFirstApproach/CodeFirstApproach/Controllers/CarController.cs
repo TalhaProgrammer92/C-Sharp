fe265c8ae05b20ab928d4262a5e93b735b9d5b47
TalhaProgrammer92 <talha.code92@gmail.com>
2025-08-03 19:00:50 +0500

    .

---
+++ ASP.NET/CodeFirstApproach/CodeFirstApproach/Controllers/CarController.cs
@@ -0,0 +1,45 @@
+﻿using CodeFirstApproach.Models;
+using CodeFirstApproach.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeFirstApproach.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        // Attribute
+        private readonly CarContext context;
+
+        // Constructor
+        public CarController(CarContext context)
+        {
+            this.context = context;
+        }
+
+        // Get: api/car
+        [HttpGet]
+        [Route("GetCars")]
+        public List<Car> GetCars()
+        {
+            // Fetching all cars from the database
+            //var cars = context.Cars.ToList();
+            //return Ok(cars);
+            return context.Cars.ToList();
+        }
+
+        // Post: api/car
+        [HttpPost]
+        [Route("AddCar")]
+        public string AddCar(Car car)
+        {
+            // Adding a new car to the database
+            context.Cars.Add(car);
+            context.SaveChanges();
+            
+            // Returning a success message
+            return "Car added successfully!";
+        }
+    }
+}
---
+++ ASP.NET/CodeFirstApproach/CodeFirstApproach/Models/CarContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstApproach.Models
+{
+    public class CarContext : DbContext
+    {
+        // Constructor
+        public CarContext(DbContextOptions<CarContext> options) : base(options)
+        {
+        }
+
+        // DbSet for Cars
+        public DbSet<Entities.Car> Cars { get; set; }
+    }
+}
