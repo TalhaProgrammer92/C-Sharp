f4aeccaac18ee25c7428505be78512a1e956ad33
TalhaProgrammer92 <talha.code92@gmail.com>
2025-08-12 07:14:41 +0500

    ;

---
+++ ASP.NET/EcommerceBackend/EcommerceBackend/DTOs/CreateOrderDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceBackend.DTOs
+{
+    public class CreateOrderDTO
+    {
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string ShippingPhone { get; set; }
+        public string ShippingAddress { get; set; }
+
+        [Required]
+        public List<OrderItemDTO> Items { get; set; };
+        public decimal Subtotal { get; set; }
+        public decimal ShippingCost { get; set; }
+        public decimal Total { get; set; }
+    }
+
+    public class OrderItemDTO
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ ASP.NET/EcommerceBackend/EcommerceBackend/Data/OrderProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using EcommerceBackend.DTOs;
+using EcommerceBackend.Models;
+using EcommerceBackend.Models.Entities;
+
+namespace EcommerceBackend.Data
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            //CreateMap<OrderItemDTO, OrderItem>()
+        }
+    }
+}
