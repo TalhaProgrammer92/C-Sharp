8d62820d6c80d99b720a6104f51da138a7119b15
TalhaProgrammer92 <talha.code92@gmail.com>
2025-08-15 23:12:16 +0500

    .

--- ASP.NET/EcommerceBackend/EcommerceBackend/Controllers/AdminController.cs
+++ ASP.NET/EcommerceBackend/EcommerceBackend/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using EcommerceBackend.Services;
+using EcommerceBackend.Models.API;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EcommerceBackend.Controllers
 {
@@ -13,6 +15,21 @@ namespace EcommerceBackend.Controllers
         public AdminController(JWTService jwtService)
         {
             _jwtService = jwtService;
+        }
+
+        // POST: api/Admin/Login
+        [AllowAnonymous]
+        [HttpPost("Login")]
+        public async Task<ActionResult<LoginResponseModel>> Login(LoginRequestModel request)
+        {
+            var response = await _jwtService.Authenticate(request);
+            
+            if (response is null)
+            {
+                return Unauthorized(new { message = "Unauthorized Access!" });
+            }
+
+            return response;
         }
     }
 }
