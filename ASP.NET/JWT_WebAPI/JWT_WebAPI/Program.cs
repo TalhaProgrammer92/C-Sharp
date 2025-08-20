3ee0591112bb38fd3c74559c4ef461479c4ecbfc
TalhaProgrammer92 <talha.code92@gmail.com>
2025-08-20 12:29:03 +0500

    .

--- ASP.NET/JWT_WebAPI/JWT_WebAPI/Program.cs
+++ ASP.NET/JWT_WebAPI/JWT_WebAPI/Program.cs
@@ -1,7 +1,20 @@
 using JWT_WebAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Create some secret bytes for issuer signing key in JWT Authentication
+byte[] bytes = new byte[64];
+using (var random = RandomNumberGenerator.Create())
+{
+    random.GetBytes(bytes);
+}
+string key = Convert.ToBase64String(bytes);
 
 // Add services to the container.
 
@@ -10,9 +23,39 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Some Configuration
+// DB Connection Configuration
 builder.Services.AddDbContext<AppDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
+
+// Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = "Issuer",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+        };
+    });
+
+// Authorization
+builder.Services.AddAuthentication();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>(options =>
+{
+    options.Password.RequiredLength = 6;    // Minimum password length
+    options.Password.RequireNonAlphanumeric = false;    // Non-alphanumeric are not required
+    options.Password.RequireDigit = false;      // Digits are not required
+    options.Password.RequireUppercase = false;  // Uppercase alphabets are not required
+    options.Password.RequireLowercase = false;  // Lowercase alphabets are not required
+})
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
@@ -22,6 +65,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Mapping - JWT
+app.MapIdentityApi<IdentityUser>();
 
 app.UseHttpsRedirection();
 
