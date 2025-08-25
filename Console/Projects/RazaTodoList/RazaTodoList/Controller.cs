06a9710520aa1d123a88f0e09ce4cf37ea0be682
TalhaProgrammer92 <talha.code92@gmail.com>
2025-08-25 23:32:16 +0500

    .

---
+++ Console/Projects/RazaTodoList/RazaTodoList/Controller.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazaTodoList
+{
+    class Controller
+    {
+        // Attributes
+        public TodoList todoList; // A controller contains a todo list
+        Section? activeSection;
+
+        // Constructor
+        public Controller()
+        {
+            todoList = new TodoList();
+            activeSection = null;
+        }
+
+        // Method - Hold the screen
+        void HoldScreen(string message = "")
+        {
+            Console.WriteLine(message + "\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        // Method - Start the application
+        public void StartApplication()
+        {
+            while (true)
+            {
+                HandleMainMenuChoice();
+            }
+        }
+
+        // Method - Handle Main Menu choices
+        void HandleMainMenuChoice()
+        {
+            Console.Clear();
+            Menu.MainMenu();
+            int choice = Menu.TakeInput(1, 5);
+
+            switch (choice)
+            {
+                case 1:
+                    ViewSection();
+                    break;
+                case 2:
+                    ViewSections();
+                    break;
+                case 3:
+                    AddSection();
+                    break;
+                case 4:
+                    RemoveSection();
+                    break;
+                case 5:
+                    SelectSection();
+                    break;
+                case 6:
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+        private void ViewSection()
+        {
+            if (todoList.ActiveSectionId != -1)
+            {
+                HandleSectionChoice(todoList.ActiveSectionId);
+            }
+            else
+            {
+                HoldScreen("No active section. Please add and select a section first.");
+            }
+        }
+
+        private void SelectSection()
+        {
+            Console.WriteLine("Enter section id: ");
+            int sectionId = Convert.ToInt32(Console.ReadLine());
+
+            todoList.ChangeActiveSection(sectionId);
+        }
+
+        private void RemoveSection()
+        {
+            Console.WriteLine("Enter section id: ");
+            int sectionId = Convert.ToInt32(Console.ReadLine());
+
+            todoList.RemoveSection(sectionId);
+        }
+
+        private void AddSection()
+        {
+            Section newSection = new Section();
+
+            Console.WriteLine("Enter section name: ");
+            newSection.Name = Console.ReadLine() ?? "";
+            newSection.Id = todoList.Sections.Count + 1;
+
+            todoList.AddSection(newSection);
+
+            HoldScreen("Section added successfully!");
+        }
+
+        // Method - Handle Section choices
+        void HandleSectionChoice(int sectionID)
+        {
+            activeSection = todoList.Sections.Find(s => s.Id == sectionID);
+            
+            if (activeSection == null)
+            {
+                Console.WriteLine($"Section with ID {sectionID} not found.");
+                HoldScreen();
+                return;
+            }
+
+            while (true)
+            {
+                Console.Clear();
+                Menu.SectionMenu(activeSection.Name!);
+                int choice = Menu.TakeInput(1, 5);
+                switch (choice)
+                {
+                    case 1:
+                        activeSection.DisplayAllTasks();
+                        break;
+                    case 2:
+                        activeSection.AddTask();
+                        break;
+                    case 3:
+                        RemoveTask();
+                        break;
+                    case 4:
+                        UpdateTask();
+                        break;
+                    case 5:
+                        activeSection.MarkAllAsCompleted();
+                        break;
+                    case 6:
+                        activeSection.MarkAllAsPending();
+                        break;
+                    case 7:
+                        return; // Back to Main Menu
+                    default:
+                        HoldScreen("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        private void UpdateTask()
+        {
+            Console.WriteLine("Enter task id: ");
+            int taskId = Convert.ToInt32(Console.ReadLine());
+
+            activeSection!.UpdateTask(taskId);
+        }
+
+        private void RemoveTask()
+        {
+            Console.WriteLine("Enter task id: ");
+            int taskId = Convert.ToInt32(Console.ReadLine());
+
+            activeSection!.RemoveTask(taskId);
+        }
+
+        private void ViewSections()
+        {
+            Console.Clear();
+            
+            foreach (var section in todoList.Sections)
+            {
+                Console.WriteLine(section);
+            }
+
+            HoldScreen();
+        }
+    }
+}
---
+++ Console/Projects/RazaTodoList/RazaTodoList/Section.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace RazaTodoList
+{
+    class Section
+    {
+        // Attributes
+        List<Task> Tasks;
+        public string? Name { get; set; } = null;
+        public int Id { get; set; }
+
+        // Constructor
+        public Section(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Tasks = new List<Task>();
+        }
+        public Section() 
+        {
+            Tasks = new List<Task>();
+        }
+
+        // Method - Hold Screen
+        void HoldScreen(string message = "")
+        {
+            Console.WriteLine(message + "\nPress any key to continue...");
+            Console.ReadKey();
+        }
+
+        // Method - Add a task to the section
+        public void AddTask()
+        {
+            Task newTask = new Task();
+
+            Console.WriteLine("Enter task description: ");
+            newTask.Description = Console.ReadLine() ?? "";
+            newTask.Id = Tasks.Count + 1;
+
+            HoldScreen("Task added successfully!");
+        }
+
+        // Method - Remove a task from the section by its ID
+        public void RemoveTask(int taskId)
+        {
+            Tasks.RemoveAll(t => t.Id == taskId);   // Fastest way to remove a task rather then using loop!
+
+            HoldScreen("Task removed successfully!");
+        }
+
+        // Method - Update a task's attributes by its ID
+        public void UpdateTask(int taskId)
+        {
+            Task? taskToUpdate = Tasks.Find(t => t.Id == taskId);
+            
+            if (taskToUpdate != null)
+            {
+                taskToUpdate = new Task();
+
+                Console.WriteLine("Enter new task description: ");
+                taskToUpdate.Description = Console.ReadLine() ?? taskToUpdate.Description;
+
+                Console.WriteLine("Completion Status (0, 1): ");
+                taskToUpdate.IsCompleted = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+
+                HoldScreen("Task updated successfully!");
+            }
+            else
+            {
+                HoldScreen($"Task with ID {taskId} not found.");
+            }
+        }
+
+        // Method - Mark all as completed
+        public void MarkAllAsCompleted()
+        {
+            foreach (var task in Tasks)
+            {
+                task.IsCompleted = true;
+            }
+
+            HoldScreen("All tasks marked as completed!");
+        }
+
+        // Method - Mark all as pending
+        public void MarkAllAsPending()
+        {
+            foreach (var task in Tasks)
+            {
+                task.IsCompleted = false;
+            }
+
+            HoldScreen("All tasks marked as pending!");
+        }
+
+        // Method - Display section information
+        public override string ToString()
+        {
+            return $"Id:\t{Id}\nSection:\t{Name}\nTotal Tasks:\t{Tasks.Count}";
+        }
+
+        // Method - Display all tasks
+        public void DisplayAllTasks()
+        {
+            Console.WriteLine($"Section: {Name} (ID: {Id})");
+            
+            foreach (var task in Tasks)
+            {
+                Console.WriteLine(task);
+                Console.WriteLine("--------------------");
+            }
+
+            if (Tasks.Count == 0)
+            {
+                HoldScreen("No tasks available in this section.");
+            }
+            else
+            {
+                HoldScreen();
+            }
+        }
+
+        // Method - Display a particular task by its ID
+        public void DisplayTask(int taskId)
+        {
+            Task? taskToDisplay = Tasks.FirstOrDefault(t => t.Id == taskId);
+            
+            if (taskToDisplay != null)
+            {
+                Console.WriteLine(taskToDisplay);
+                HoldScreen();
+            }
+            else
+            {
+                HoldScreen($"Task with ID {taskId} not found.");
+            }
+        }
+
+        // Method - Display all pending tasks
+        public void DisplayPendingTasks()
+        {
+            var pendingTasks = Tasks.Where(t => !t.IsCompleted).ToList();   // Returns a list of pending tasks
+            Console.WriteLine($"Pending Tasks in Section: {Name} (ID: {Id})");
+            
+            foreach (var task in pendingTasks)
+            {
+                Console.WriteLine(task);
+                Console.WriteLine("--------------------");
+            }
+
+            if (pendingTasks.Count == 0)
+            {
+                HoldScreen("No pending tasks available in this section.");
+            }
+            else
+            {
+                HoldScreen();
+            }
+        }
+
+        // Method - Display all completed tasks
+        public void DisplayCompletedTasks()
+        {
+            var completedTasks = Tasks.Where(t => t.IsCompleted).ToList();   // Returns a list of completed tasks
+            Console.WriteLine($"Completed Tasks in Section: {Name} (ID: {Id})");
+
+            foreach (var task in completedTasks)
+            {
+                Console.WriteLine(task);
+                Console.WriteLine("--------------------");
+            }
+
+            if (completedTasks.Count == 0)
+            {
+                HoldScreen("No completed tasks available in this section.");
+            }
+            else
+            {
+                HoldScreen();
+            }
+        }
+    }
+}
