af8618d2bdb21a4ed3a7789a856c51a0691a25b3
TalhaProgrammer92 <talha.code92@gmail.com>
2025-09-03 16:05:01 +0500

    .

---
+++ Console/Projects/TicTacToe/TicTacToe/Player/Player.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Player
+{
+    public class Player
+    {
+        // Attributes
+        public Name? Name { get; set; }
+        public Score? Score { get; set; }
+
+        // Constructor
+        public Player()
+        {
+            Name = null;
+            Score = null;
+        }
+        public Player(Name name, Score score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        // Representation method
+        public override string ToString()
+        {
+            return $"Name:\t{Name!.ToString()}\nScore:\t{Score!.ToString()}";
+        }
+    }
+}
---
+++ Console/Projects/TicTacToe/TicTacToe/Player/Score.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Player
+{
+    public class Score
+    {
+        // Attributes
+        int score;
+
+        // Constructor
+        public Score()
+        {
+            score = 0;
+        }
+        public Score(int score)
+        {
+            this.score = score;
+        }
+
+        // Getter
+        public int Get()
+        {
+            return score;
+        }
+
+        // Method - Increment
+        public void Increase(int increment = 1)
+        {
+            this.score += increment;
+        }
+
+        // Method - Reset
+        public void Reset()
+        {
+            this.score = 0;
+        }
+
+        // Representation method
+        public override string ToString()
+        {
+            return score.ToString();
+        }
+    }
+}
